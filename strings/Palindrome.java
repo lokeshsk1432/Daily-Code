cd91cb1eed51d2546e64eada38927cb79dd3049b
lokesh shrikhande <164716278+lokeshsk1432@users.noreply.github.com>
2026-04-01 17:07:52 +0530

    Update Palindrome.java

--- strings/Palindrome.java
+++ strings/Palindrome.java
@@ -1,1 +1,22 @@
+// Problem: Check if a string is palindrome
+// Approach: Reverse string and compare OR two pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+    import java.util.Scanner;
+
+    public class PalindromeString {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Word");
+            String input = sc.next();
+            String reversed = new StringBuilder(input).reverse().toString();
+            if(input.equals(reversed)){
+                System.out.println("is Palindrome");
+            }else{
+                System.out.println("is Non Palindrome");
+            }
+        }
+    }
 
